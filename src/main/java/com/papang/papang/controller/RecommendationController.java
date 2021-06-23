6a0f3ca2aa59c6bff5c1dda19278a5866ae4aef2
khedmsl1028 <khedmsl1028@daum.net>
2021-06-23 22:17:36 +0900

    [추천] 향수 추천 선택안함 업데이트
    선택안함 선택 시 메인, 첫번째 향료만으로 추천해주는 기능 추가

--- src/main/java/com/papang/papang/controller/RecommendationController.java
+++ src/main/java/com/papang/papang/controller/RecommendationController.java
@@ -25,4 +25,13 @@ public class RecommendationController {
         return p_repo.findByRecommendationPerfume(concentration, size1, size2, style, main, first, second);
     }
 
+    @GetMapping(value = "/{concentration}/{size1}/{size2}/{style}/{main}/{first}", produces = "application/json; charset=utf-8")
+    public ArrayList<String> getPerfumeRecommendation3(@PathVariable("concentration") int concentration,
+                                                       @PathVariable("size1") int size1, @PathVariable("size2") int size2,
+                                                       @PathVariable("style") int style, @PathVariable("main") int main,
+                                                       @PathVariable("first") int first){
+
+        return p_repo.findByRecommendationPerfumeExclude(concentration, size1, size2, style, main, first);
+    }
+
 }
--- src/main/java/com/papang/papang/repository/PerfumeRepository.java
+++ src/main/java/com/papang/papang/repository/PerfumeRepository.java
@@ -19,6 +19,12 @@ public interface PerfumeRepository extends JpaRepository<Perfume, Integer> {
     ArrayList<String> findByRecommendationPerfume(int concentration, int size1, int size2, int style,
                                                    int main, int first, int second);
 
+    @Query(value = "SELECT d.perfumeID, d.name, d.brand FROM perfume d " +
+            "WHERE d.concentration = :concentration AND d.size between :size1 AND :size2 " +
+            "AND d.style = :style AND d.main = :main AND d.first = :first ORDER BY RAND() LIMIT 5", nativeQuery = true)
+    ArrayList<String> findByRecommendationPerfumeExclude(int concentration, int size1, int size2, int style,
+                                                  int main, int first);
+
     @Query("select p from Perfume p where p.name = :name")
     List<Perfume> findByName(String name);
 }
